379441df534c79d338872535cdeb1e47fafd5284
jaayap <lebert.jasmine@gmail.com>
2018-04-03 18:25:40 +0200

    add respawn script for object

---
+++ Assets/Jasmine/Scripts/Respawn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+
+    public GameObject formes;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(
+                other.tag == "square"
+                || other.tag == "star"
+                || other.tag == "Rond"
+                || other.tag == "Triangle"
+                || other.tag == "heart"
+            )
+        {
+            other.gameObject.transform.position = formes.transform.position;
+        }
+    }
+}
